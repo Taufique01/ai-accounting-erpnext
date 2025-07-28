6897fabfe1bc47a9e700cee26408fd4d9641d23d
taufique01 <taufiquehr@gmail.com>
2025-07-28 19:55:51 +0600

    for less confidence

--- ai_accountant/ai_accountant/llm.py
+++ ai_accountant/ai_accountant/llm.py
@@ -83,7 +83,7 @@ def classify_transaction(tx_list, status="Pending"):
         {
             "role": "system",
             "content": (
-                "Midwest service bureau, LLC is a Technology-Enhanced Debt Recovery with Human Touch",
+                "Midwest service bureau, LLC is a Technology-Enhanced Debt Recovery with Human Touch company.",
                 "You are an expert accountant of the company. Your task is to classify bank transactions into double-entry journal entries. "
                 "For each transaction, return the corresponding 'debit_account' and 'credit_account' using the company's Chart of Accounts below. Use the name of the account as 'debit_account' and 'credit_account'"
                 "Only use account names from the list.You can also use vendor details if available. If no suitable match is found or you are not confident, return 'Unknown account name' for debit or credit."
@@ -235,6 +235,11 @@ def save_journal_entry(result, created_at_str):
         amount = entry.get("amount")
         memo = entry.get("memo", "")
         counterparty = entry.get("counterparty", "")
+        confidence = entry.get("confidence")
+        
+        if confidence < 7.5:
+            raise ValueError(f"Classification confidence is very less {confidence}")
+
             
         # Get party info for debit and credit accounts
         party_for_debit = get_party_info(debit_account, counterparty)
