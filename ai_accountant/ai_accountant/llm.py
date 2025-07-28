2c9013962caae057419c7906b6935e7e31c4989c
taufique01 <taufiquehr@gmail.com>
2025-07-29 01:41:58 +0600

    remove model name

--- ai_accountant/ai_accountant/llm.py
+++ ai_accountant/ai_accountant/llm.py
@@ -83,7 +83,7 @@ def classify_transaction(tx_list, status="Pending"):
         {
             "role": "system",
             "content": (
-                "Midwest service bureau, LLC is a Technology-Enhanced Debt Recovery with Human Touch company.",
+                "Midwest service bureau, LLC is a Technology-Enhanced Debt Recovery with Human Touch company."
                 "You are an expert accountant of the company. Your task is to classify bank transactions into double-entry journal entries. "
                 "For each transaction, return the corresponding 'debit_account' and 'credit_account' using the company's Chart of Accounts below. Use the name of the account as 'debit_account' and 'credit_account'"
                 "Only use account names from the list.You can also use vendor details if available. If no suitable match is found or you are not confident, return 'Unknown account name' for debit or credit."
@@ -95,14 +95,14 @@ def classify_transaction(tx_list, status="Pending"):
         }
     ]
 
-    model = "gpt-3.5-turbo"    
+    model = "gpt-4o"   
     if status == "Error":
         prompt.append({
             "role": "system",
             "content": ERROR_PROMPT
         })
         
-        model = "gpt-4o"
+        
     
     
     prompt.append(        {
